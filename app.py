71ac67cd277a1d0a1cf4ec66a3a271967f2d0069
yasshass <32908418+yasshass@users.noreply.github.com>
2017-10-20 15:21:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -51,15 +51,15 @@ def processRequest(req):
     parameters=contexts[0].get("parameters")
     fonction=parameters.get("Fonction")
     
-#     baseurl = "http://52.15.100.135:5000/"
-#     if fonction=="tags":
-#         url=baseurl+"tags/"
-#     if fonction=="articles similaires":
-#         url=baseurl+"similar_articles/"
-#     reponse = put(url, 
-#                data={"text": text})
-#     data=reponse.json()
-    data=fonction
+    baseurl = "http://52.15.100.135:5000/"
+    if fonction=="tags":
+        url=baseurl+"tags/"
+    if fonction=="articles similaires":
+        url=baseurl+"similar_articles/"
+    reponse = put(url, 
+               data={"text": text})
+    data=reponse.json()
+    #data=fonction
     #data = json.loads(result)
     res = makeWebhookResult(data)
     return res
